2c3c193b41936d2527bc5e2585cef58127891656
Meo So <meowsoso@gmail.com>
2019-09-07 18:38:07 +1000

    raining effect

--- js/rain.js
+++ js/rain.js
@@ -1,10 +1,13 @@
 let acceleration = 0.0098;
-let nDrops = 400;
+let nDrops = 500;
 let drops = [];
 let song;
 let button;
 let amp;
 let vol = 0;
+let pg;
+// let width = window.innerWidth;
+// let height = window.innerHeight;
 
 function toggleSong() {
   if (song.isPlaying()) {
@@ -16,14 +19,23 @@ function toggleSong() {
 
 function preload() {
   song = loadSound("../audio/Helios - Nothing It Can.mp3");
+  bg = loadImage("../img/Water_Lilies.jpg");
+  //   img = loadImage("http://placekitten.com/960/924");
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(960, 650);
+  //   pg = createGraphics(960, 700);
+
+  //   image(img, 0, 0);
+  //   pond.id = "lilyPond";
   button = createButton("toggle");
   button.mousePressed(toggleSong);
+
+  // play music and get amp
   song.play();
   amp = new p5.Amplitude();
+  // add rain lines
   for (i = 0; i < nDrops; i++) {
     drops.push(new Drop());
   }
@@ -31,23 +43,28 @@ function setup() {
 
 // check if vol change more than 20%
 function isChange(volLevel) {
-  console.log(Math.abs(volLevel - vol) / vol);
+  //   console.log(Math.abs(volLevel - vol) / vol);
   return Math.abs(volLevel - vol) / vol > 0.4;
 }
 
 function draw() {
   clear();
+  //   background(bg);
   let volLevel = amp.getLevel();
   if (isChange(volLevel)) {
     vol = volLevel;
-    nDrops = map(vol, 0, 1, 0, 300);
+    nDrops = map(vol, 0, 1, 0, 500);
   }
   //   console.log(vol);
   for (let i = 0; i < nDrops; i++) {
     drops[i].drawAndDrop();
   }
+
+  // background fadeout
+  drawDot(random(width), random(height));
 }
 
+// draw lines as raindrops
 function Drop() {
   this.initX = function() {
     this.x = random() * width;
@@ -84,3 +101,48 @@ function Drop() {
     }
   };
 }
+
+/////////////////// background fadeout effect //////////////////
+
+let backgroundCover = document.getElementById("bgCover"),
+  bgCanvas = backgroundCover.getContext("2d"),
+  brushRadius = backgroundCover.width / 50,
+  img = new Image();
+console.log(bgCanvas);
+
+img.src = "../img/woman_son.jpg";
+img.onload = function() {
+  bgCanvas.imageSmoothingEnabled = false;
+  bgCanvas.drawImage(img, 0, 0, backgroundCover.width, backgroundCover.height);
+  console.log(window.devicePixelRatio);
+};
+
+// make canvas disappear
+function drawDot(mouseX, mouseY) {
+  bgCanvas.beginPath();
+  bgCanvas.arc(mouseX, mouseY, brushRadius, 0, 2 * Math.PI, true);
+  bgCanvas.fillStyle = "#000";
+  bgCanvas.globalCompositeOperation = "destination-out";
+  bgCanvas.fill();
+}
+
+function getBrushPos(xRef, yRef) {
+  // the clicking area
+  const bgRect = backgroundCover.getBoundingClientRect();
+  return {
+    x: Math.floor(
+      ((xRef - bgRect.left) / (bgRect.right - bgRect.left)) *
+        backgroundCover.width
+    ),
+    y: Math.floor(
+      ((yRef - bgRect.top) / (bgRect.bottom - bgRect.top)) *
+        backgroundCover.height
+    )
+  };
+}
+backgroundCover.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log("clicked");
+  const brushPos = getBrushPos(e.clientX, e.clientY);
+  drawDot(brushPos.x, brushPos.y);
+});
--- js/ripple.js
+++ js/ripple.js
@@ -6,30 +6,11 @@ $(document).ready(function() {
       dropRadius: 20,
       perturbance: 0.02
     });
-    // $("main").ripples({
-    //   resolution: 128,
-    //   dropRadius: 10,
-    //   perturbance: 0.04,
-    //   interactive: false
-    // });
   } catch (e) {
     $(".error")
       .show()
       .text(e);
   }
-
-  //   $(".js-ripples-disable").on("click", function() {
-  //     $("body, main").ripples("destroy");
-  //     $(this).hide();
-  //   });
-
-  //   $(".js-ripples-play").on("click", function() {
-  //     $("body, main").ripples("play");
-  //   });
-
-  //   $(".js-ripples-pause").on("click", function() {
-  //     $("body, main").ripples("pause");
-  //   });
 
   //   $("body").ripples("drop", 400, 400, 20, 5);
 });
